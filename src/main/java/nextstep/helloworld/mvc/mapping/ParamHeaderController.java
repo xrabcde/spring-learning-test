0acbb51b03c562dd024d3e6f8c624e4008949573
xrabcde <xrabcde@gmail.com>
2021-04-14 11:28:16 +0900

    feat(ParamHeaderController): Parameters와 headers 테스트

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -2,6 +2,7 @@ package nextstep.helloworld.mvc.mapping;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping("/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
+    @GetMapping(path = "/message", params = "name=hello")
     public ResponseEntity<String> messageForParam() {
         return ResponseEntity.ok().body("hello");
     }
 
+    @GetMapping(path = "/message", headers = "HEADER=hi")
     public ResponseEntity<String> messageForHeader() {
         return ResponseEntity.ok().body("hi");
     }
